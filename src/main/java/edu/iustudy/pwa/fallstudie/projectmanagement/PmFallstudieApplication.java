2be79961382fe592e90c6e50f42d9d9e35ac929e
AndreasCoors <andreas.coors@attempto.eu>
2022-04-04 14:56:21 +0200

    Initial commit

---
+++ src/main/java/edu/iustudy/pwa/fallstudie/projectmanagement/PmFallstudieApplication.java
@@ -0,0 +1,13 @@
+package edu.iustudy.pwa.fallstudie.projectmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmFallstudieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmFallstudieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/iustudy/pwa/fallstudie/projectmanagement/PmFallstudieApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.iustudy.pwa.fallstudie.projectmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmFallstudieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
