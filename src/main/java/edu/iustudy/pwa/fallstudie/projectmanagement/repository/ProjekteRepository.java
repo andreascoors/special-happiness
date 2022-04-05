97046d8316c3d5ff7a0371dc879f01a3f489f332
AndreasCoors <andreas.coors@attempto.eu>
2022-04-05 06:52:12 +0200

    Projekte CRUD-repo

---
+++ src/main/java/edu/iustudy/pwa/fallstudie/projectmanagement/repository/ProjekteRepository.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package edu.iustudy.pwa.fallstudie.projectmanagement.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import edu.iustudy.pwa.fallstudie.projectmanagement.db.Projekte;
+
+/**
+ * @author AndreasCoors
+ *
+ */
+public interface ProjekteRepository extends CrudRepository<Projekte, Long> {
+
+}
