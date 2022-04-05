5f68bc630dfd02322f6c4fe0cdd056b0cc71d9f7
AndreasCoors <andreas.coors@attempto.eu>
2022-04-05 07:06:23 +0200

    basic service setup

---
+++ src/main/java/edu/iustudy/pwa/fallstudie/projectmanagement/service/PmFallstudieService.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package edu.iustudy.pwa.fallstudie.projectmanagement.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.iustudy.pwa.fallstudie.projectmanagement.db.Projekte;
+import edu.iustudy.pwa.fallstudie.projectmanagement.repository.ArbeitspaketeRepository;
+import edu.iustudy.pwa.fallstudie.projectmanagement.repository.MitarbeiterRepository;
+import edu.iustudy.pwa.fallstudie.projectmanagement.repository.ProjekteRepository;
+
+/**
+ * @author AndreasCoors
+ *
+ */
+@Service
+@Transactional
+public class PmFallstudieService {
+
+	@Autowired
+	ProjekteRepository projektRepo;
+	@Autowired
+	ArbeitspaketeRepository apRepo;
+	@Autowired
+	MitarbeiterRepository mitarbeiterRepo;
+
+	Iterable<Projekte> findeAlleProjekte() {
+
+		return projektRepo.findAll();
+
+	}
+}
