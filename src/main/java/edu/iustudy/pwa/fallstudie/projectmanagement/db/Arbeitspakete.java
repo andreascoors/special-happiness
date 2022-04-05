dd9559100aea3e78c93f7509cd6adacf72898082
AndreasCoors <andreas.coors@attempto.eu>
2022-04-05 04:07:38 +0200

    Arbeitspakete Tabelle

--- src/main/java/edu/iustudy/pwa/fallstudie/projectmanagement/db/Arbeitspakete.java
+++ src/main/java/edu/iustudy/pwa/fallstudie/projectmanagement/db/Arbeitspakete.java
@@ -3,11 +3,13 @@
  */
 package edu.iustudy.pwa.fallstudie.projectmanagement.db;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,14 +29,15 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class Mitarbeiter{
+public class Arbeitspakete {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@ManyToOne
-	private Long id;
-	private String vorname;
-	private String nachname;
-	private java.sql.Date geburtsdatum;
-	// TODO ... weitere
-
+	private long id;
+	private String name;
+	private String beschreibung;
+	private java.sql.Date startDatum;
+	private java.sql.Date endDatum;
+	private int aufwand;
+	@OneToMany
+	private List<Mitarbeiter> mitarbeiter;
 }
