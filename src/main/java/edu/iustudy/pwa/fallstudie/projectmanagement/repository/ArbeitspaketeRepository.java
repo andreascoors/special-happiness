91693c3bccd5bca86777fd96cfe3ecd0b227e31f
AndreasCoors <andreas.coors@attempto.eu>
2022-04-05 06:50:15 +0200

    Arbeitspakete CRUD-repo

---
+++ src/main/java/edu/iustudy/pwa/fallstudie/projectmanagement/repository/ArbeitspaketeRepository.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package edu.iustudy.pwa.fallstudie.projectmanagement.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import edu.iustudy.pwa.fallstudie.projectmanagement.db.Arbeitspakete;
+
+/**
+ * @author AndreasCoors
+ *
+ */
+public interface ArbeitspaketeRepository extends CrudRepository<Arbeitspakete, Long> {
+
+}
