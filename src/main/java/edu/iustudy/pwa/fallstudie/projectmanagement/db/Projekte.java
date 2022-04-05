31a9cf784c4776d41a44e53c55cc637c990ef558
AndreasCoors <andreas.coors@attempto.eu>
2022-04-05 04:16:51 +0200

    Projekt Tabelle

--- src/main/java/edu/iustudy/pwa/fallstudie/projectmanagement/db/Projekte.java
+++ src/main/java/edu/iustudy/pwa/fallstudie/projectmanagement/db/Projekte.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
@@ -29,15 +28,16 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class Arbeitspakete {
+public class Projekte {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue
 	private long id;
 	private String name;
 	private String beschreibung;
 	private java.sql.Date startDatum;
 	private java.sql.Date endDatum;
-	private int aufwand;
 	@OneToMany
-	private List<Mitarbeiter> verantwortlich;
+	private List<Mitarbeiter> zugehörig;
+	
+	
 }
