fe185b005360a5bc75928bb85c91468583357a05
AndreasCoors <andreas.coors@attempto.eu>
2022-04-05 06:51:24 +0200

    Mitarbeiter CRUD repository

---
+++ src/main/java/edu/iustudy/pwa/fallstudie/projectmanagement/repository/MitarbeiterRepository.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package edu.iustudy.pwa.fallstudie.projectmanagement.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import edu.iustudy.pwa.fallstudie.projectmanagement.db.Mitarbeiter;
+
+/**
+ * @author AndreasCoors
+ *
+ */
+public interface MitarbeiterRepository extends CrudRepository<Mitarbeiter, Long> {
+
+}
